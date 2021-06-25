13353b7e121979b31d5c77213067916440c26987
Jubar007 <1403600882@qq.com>
2021-06-25 15:56:55 +0800

    Revert "Merge branch 'yang' of https://github.com/Jubar007/IMsoftware into yang"
    This reverts commit 695bd31ed6a5e6be82a189789b199f3526908c11, reversing
    changes made to 1d3dfacde6c482b38a3e6d94547955a8cc3920d2.

--- logindialog.cpp
+++ logindialog.cpp
@@ -11,6 +11,7 @@
 #include "mainwindow.h"
 #include "sqlite.h"
 
+
 LoginDialog::LoginDialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::LoginDialog)
@@ -65,6 +66,7 @@ void LoginDialog::showChatWindow()
     QString uname=ui->usrLineEdit->text();
     QString password=ui->pwdLineEdit->text();
     QString match_password;
+        int im = 0;
     if(db->db_query("select * from User"))
     {
           //qDebug()<<"读取成功";
@@ -81,6 +83,8 @@ void LoginDialog::showChatWindow()
                 //qDebug()<<typeid(db->query).name();//打印查询结果类型QSqlQuery
                 while(db->query.next())//一行一行遍历
                 {
+                    im++;
+                    //qDebug()<<im<<":"<<db->query.value("U_NickName").toString();
                     if(uname==db->query.value("U_NickName").toString()) {
                         seek = true;
                         //密码不正确
@@ -196,7 +200,7 @@ void LoginDialog::on_signPushButton_clicked()
 
 //登录的电话检测
 bool LoginDialog::queryphone(QString phone){
-    Sqlite *db = new Sqlite("sqlite/simpleChat.db");
+    Sqlite *db = new Sqlite("/sqlite/simpleChat.db");
     //判断数据库中是否存在所输入号码
     QString sql = "select * from User  where U_Telephone='"+phone+"'";
     db->db_query(sql);
